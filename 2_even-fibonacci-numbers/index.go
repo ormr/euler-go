e23a28b83eecd31515dc5b02fcb7396a9c265499
Serafim Gavrilov <seraf.gavrilov@gmail.com>
2020-08-04 23:47:01 +0300

    2-even-fibonacci-numbers

---
+++ 2_even-fibonacci-numbers/index.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(fiboEvenSum(10))
+	fmt.Println(fiboEvenSum(60))
+	fmt.Println(fiboEvenSum(1000))
+	fmt.Println(fiboEvenSum(100000))
+	fmt.Println(fiboEvenSum(4000000))
+}
+
+func fiboEvenSum(n int) int {
+	sum := 0
+	Fa := 1
+	Fb := 1
+	for Fb <= n {
+		Fc := Fa + Fb
+		Fa = Fb
+		Fb = Fc
+
+		if (Fb % 2 == 0) {
+			sum += Fb
+		}
+	}
+	return sum
+}
