b6c400d01392914d04e95bd30c912ca380968f2e
Serafim Gavrilov <seraf.gavrilov@gmail.com>
2020-08-14 12:44:56 +0300

    4 largest palindrome product

--- 4_largest-palindrome-product/index.go
+++ 4_largest-palindrome-product/index.go
@@ -3,9 +3,51 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hi!")
+	fmt.Println(largestPalindromeProduct(2))
+	fmt.Println(largestPalindromeProduct(3))
+}
+
+func reverseInteger(n int) int {
+	reversedInt := 0
+
+	for n > 0 {
+		remainder := n % 10
+		reversedInt *= 10
+		reversedInt += remainder
+		n /= 10
+	}
+
+	return reversedInt
+}
+
+func isPalindrome(n int) bool {
+	return n == reverseInteger(n)
 }
 
 func largestPalindromeProduct(n int) int {
-	
+	largestPalindrome := 0
+	maxDigit := 1
+
+	for i := 0; i < n; i++ {
+		maxDigit = maxDigit * 10
+	}
+
+	lowDigit := (maxDigit / 10)
+	maxDigit = maxDigit - 1
+
+	for i := maxDigit; i > lowDigit; i-- {
+		for j := maxDigit; j > lowDigit; j-- {
+			product := i * j
+
+			if product > largestPalindrome {
+				if (isPalindrome(product)) {
+					largestPalindrome = product
+				}
+			} else if (product < largestPalindrome) {
+				break
+			}
+		}
+	}
+
+	return largestPalindrome
 }
